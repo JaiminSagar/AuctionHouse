c66834bcbc0244c187fd417618dd6736cdafb6c9
Darshil <dpatel0599@gmail.com>
2020-03-15 17:47:34 +0530

    done venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!J:\Last Year Project\AuctionHouse (15-03-2020)\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
